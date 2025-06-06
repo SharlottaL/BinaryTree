2332edd75c0f9629914e18c0454cabbfadb0a651
SharlottaL <botanichkagigantika@gmail.com>
2025-06-06 15:53:40 +0300

    Balance DONE

---
+++ TreePerfomanse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace BinaryTree
+{
+    internal class TreePerfomanse<T>
+    {
+        public delegate T Method();
+        public static void Measure(string message, Method method)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            T value = method();
+            sw.Stop();
+            Console.WriteLine($"{message}{value}, вычислино за {sw.Elapsed.ToString("ss\\.fffffff")}");
+        }
+    }
+}
